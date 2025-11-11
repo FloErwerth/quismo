5daa0d1a181260bcf1cd8081c58c88b0d9cfd65d
florianerwerth <florian.erwerth@ambient.digital>
2025-11-11 13:35:00 +0100

    finalize first boilerplate of bottom menu

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,4 +1,4 @@
-import { CalendarCheck, Cog, House } from "@tamagui/lucide-icons";
+import { ChevronUp, Cog, House, X } from "@tamagui/lucide-icons";
 import { Tabs } from "expo-router";
 import { useState } from "react";
 import { useWindowDimensions } from "react-native";
@@ -6,9 +6,11 @@ import { Gesture, GestureDetector } from "react-native-gesture-handler";
 import Animated, {
 	useAnimatedStyle,
 	useSharedValue,
+	withDelay,
 	withSpring,
+	withTiming,
 } from "react-native-reanimated";
-import { AnimatePresence, Card, SizableText, useTheme, View } from "tamagui";
+import { AnimatePresence, Card, useTheme, View } from "tamagui";
 import { FloatingView } from "@/components/FloatingView";
 
 const MAX_PANNING_DISTANCE_X = 6;
@@ -28,6 +30,31 @@ const ContextMenu = () => {
 	const [isContextMenuOpen, setIsContextMenuOpen] = useState(false);
 	const screen = useWindowDimensions();
 	const contextMenuWidth = Math.min(screen.width - 32, 300);
+
+	const bottomClose = useSharedValue(30);
+	const bottomScale = useSharedValue(0);
+	const bottomOpacity = useSharedValue(0);
+	const bottomRotation = useSharedValue(150);
+
+	const closeButton = useAnimatedStyle(() => {
+		return {
+			position: "absolute",
+			width: 45,
+			height: 45,
+			backgroundColor: "red",
+			alignSelf: "center",
+			borderRadius: 50,
+			justifyContent: "center",
+			alignItems: "center",
+			zIndex: -1,
+			bottom: bottomClose.value,
+			transform: [
+				{ rotate: `${bottomRotation.value}deg` },
+				{ scale: bottomScale.value },
+			],
+			opacity: bottomOpacity.value,
+		};
+	});
 
 	const pan = Gesture.Pan()
 		.onBegin(() => {
@@ -87,6 +114,11 @@ const ContextMenu = () => {
 		bottom.value = withSpring(0, { duration: OPEN_ANIMATION_DURATION });
 		left.value = withSpring(0, { duration: OPEN_ANIMATION_DURATION });
 		borderRadius.value = withSpring(100, { damping: 80, stiffness: 1200 });
+
+		bottomClose.value = withSpring(50, { damping: 80, stiffness: 1200 });
+		bottomScale.value = withSpring(0, { damping: 80, stiffness: 1200 });
+		bottomOpacity.value = withTiming(0, { duration: 75 });
+		bottomRotation.value = withTiming(15, { duration: 75 });
 	};
 
 	const openContextMenu = () => {
@@ -99,20 +131,29 @@ const ContextMenu = () => {
 		bottom.value = withSpring(50, { duration: OPEN_ANIMATION_DURATION });
 		left.value = withSpring(0, { duration: OPEN_ANIMATION_DURATION });
 		borderRadius.value = withSpring(25, { damping: 80, stiffness: 1200 });
-	};
 
-	const toggleContextMenu = () => {
-		if (isContextMenuOpen) {
-			closeContextMenu();
-		} else {
-			openContextMenu();
-		}
+		bottomClose.value = withDelay(
+			30,
+			withSpring(-10, { damping: 80, stiffness: 1200 }),
+		);
+		bottomScale.value = withDelay(
+			50,
+			withSpring(1, { damping: 80, stiffness: 1200 }),
+		);
+		bottomOpacity.value = withDelay(
+			100,
+			withSpring(1, { damping: 80, stiffness: 1200 }),
+		);
+		bottomRotation.value = withDelay(
+			200,
+			withSpring(0, { damping: 40, stiffness: 1200 }),
+		);
 	};
 
 	return (
 		<GestureDetector gesture={pan.enabled(!isContextMenuOpen)}>
 			<View
-				onPress={toggleContextMenu}
+				onPress={openContextMenu}
 				alignSelf="center"
 				animation="superBouncy"
 				position="absolute"
@@ -130,13 +171,14 @@ const ContextMenu = () => {
 								{isContextMenuOpen ? (
 									<View
 										key="context-menu-content"
-										animation="quick"
+										animation="100ms"
 										gap="$4"
 										padding="$4"
-										enterStyle={{ opacity: 0 }}
+										enterStyle={{ opacity: 0, animationDuration: 100 }}
 										exitStyle={{ opacity: 0, animationDuration: 100 }}
-										backgroundColor="transparent"
+										backgroundColor="$background"
 										width="95%"
+										zIndex={1}
 										height="95%"
 										position="absolute"
 									>
@@ -152,18 +194,22 @@ const ContextMenu = () => {
 								) : (
 									<View
 										key="context-menu-button"
-										animation="quick"
+										animation="100ms"
 										enterStyle={{ opacity: 0 }}
 										exitStyle={{ opacity: 0 }}
 									>
-										<CalendarCheck size="$1" scale={0.8} alignSelf="center" />
-										<SizableText>Open</SizableText>
+										<ChevronUp size="$2" alignSelf="center" />
 									</View>
 								)}
 							</AnimatePresence>
 						</Animated.View>
 					</FloatingView>
 				</Animated.View>
+				<View onPress={closeContextMenu}>
+					<Animated.View style={closeButton}>
+						<X />
+					</Animated.View>
+				</View>
 			</View>
 		</GestureDetector>
 	);
